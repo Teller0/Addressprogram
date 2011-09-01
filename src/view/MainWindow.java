65dea84a2e23d382e4dcbb1d471a1664ba5d9899
Bernhard F. Brodowsky <brodowsb@student.ethz.ch>
2011-09-01 11:14:26 +0200

    Introduced main window.

---
+++ src/view/MainWindow.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MainWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MainWindow() {
+		setSize(800, 400);
+		add(new JLabel("asdfasdfASDFASDFasdf"));
+		setVisible(true);
+	}
+
+}
