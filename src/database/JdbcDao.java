78e9cfae0a63ca4871fbd1f735ee0f0c3e6c72c0
Bernhard F. Brodowsky <brodowsb@student.ethz.ch>
2011-09-01 16:07:25 +0200

    Added database stuff.

---
+++ src/database/JdbcDao.java
@@ -0,0 +1,25 @@
+package database;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcDao implements IDao {
+	
+        // Some nice hard-coded values.
+        private static final String DRIVER = "org.postgresql.Driver";
+        private static final String DB_USER = "livia";
+        private static final String DB_PASSWD = "thierry";
+        private static final String DB_PATH = "jdbc:postgresql://localhost/addresses";
+
+        public JdbcDao() throws SQLException,
+                        ClassNotFoundException {
+                Class.forName(DRIVER);
+                DriverManager.getConnection(DB_PATH, DB_USER, DB_PASSWD);
+        }
+        
+        
+        public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        	IDao dao = new JdbcDao();
+        }
+
+}
